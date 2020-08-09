e836fafc7ac33b646252e9ffa842399e9841e943
Renaldi Arlin <johanes123renaldi@gmail.com>
2020-08-09 20:43:30 +0700

    Merging and rapih2

--- script.js
+++ script.js
@@ -38,14 +38,28 @@ function generateRandomTemp() {
    return Math.floor((Math.random() * 40) + 1) 
 }
 
+function generateRandomHumidity() {
+   //Generate random number from 40 to 99
+   return Math.floor(40 + (Math.random() * 60))
+}
+
+function generateRandomLight () {
+    return Math.floor((Math.random()*1000)+1)
+}
+
+function generateRandomN(){
+   //generate random number from 1 to 100 
+   return Math.floor((Math.random()*100)+1)
+}
+
 function pHIndicator(ph){
    var stat
 
-   if (ph > 6.0 && ph < 7.0) stat = "Suasana optimal"
-   else if (ph >= 3.0 && ph <= 6.0) stat = "Suasana asam"
-   else if (ph >= 7.0 && ph <= 10.0) stat = "Suasana basa"
-   else if (ph > 10.0) stat = "Suasana terlalu basa"
-   else stat = "Suasana terlalu asam"
+   if (ph > 6.0 && ph < 7.0) stat = "Suasana sudah optimal"
+   else if (ph >= 3.0 && ph <= 6.0) stat = "Suasana asam, usahakan pH pada 6 - 7"
+   else if (ph >= 7.0 && ph <= 10.0) stat = "Suasana basa, usahakan pH pada 6 - 7"
+   else if (ph > 10.0) stat = "Suasana terlalu basa, usahakan pH pada 6 - 7"
+   else stat = "Suasana terlalu asam, usahakan pH pada 6 - 7"
    
    document.getElementById('ph-indicator').innerHTML = stat
 }
@@ -60,17 +74,62 @@ function indikatorsuhu(x){
    document.getElementById('ind-suhu').innerHTML = stat
 }
 
+// Indikator kelembapan, abis searching katanya 60-90% cukup, tapi kalo mau diubah lagi sabeb sihh
+function humidityIndicator(humidity){
+   var stat
+
+   if (40 <= humidity && humidity < 60 ) stat = "Terlalu rendah, ambil tindakan!"
+   else if (60 <= humidity && humidity <= 90) stat = "Kelembapan sudah pas"
+   else stat = "Terlalu tinggi, ambil tindakan!"
+
+   document.getElementById('humidity-indicator').innerHTML = stat
+}
+
+function lightIndicator(x){
+    if (x>=1 && x<=50) return "Situasi remang-remang, sangat perlu tambahan cahaya"
+    else if (x>50 && x<=100) return "Situasi redup, perlu tambahan cahaya"
+    else if (x>100 && x<=400) return "Situasi terang, pertahankan keadaan sekarang"
+    else return "Situasi sangat Terang, dapat mengurangi intensitas cahaya"
+}
+
+function Nindicator(N){
+   if (N<10) return "Sangat rendah"
+   else if (N>=10 && N <=20) return "Rendah"
+   else if (N>20 && N <=50) return "Sedang"
+   else if (N>50 && N <= 75) return "Tinggi"
+   else return "Sangat tinggi"
+ 
+}
+
 // Infinite loop with interval 1 second
 function infiniteLoop(){
+   const light = document.getElementById('Pencahayaan (Lux)')
+   const light_indicator = document.getElementById ('Pencahayaan (Lux) - indicator')
+   const nitro = document.getElementById('N')
+   const nitroindicator = document.getElementById('N-indicator')
+
+   const amountOfLight = generateRandomLight()
+   const N = generateRandomN()
+
    //Change the temperature to a random number
    document.getElementById('suhu').innerHTML = String(generateRandomTemp()+"℃")
    document.getElementById('ph').innerHTML = String(generateRandompH() )
+   document.getElementById('humidity').innerHTML = String(generateRandomHumidity() + "%")
+
+   light.innerHTML = String(amountOfLight)
+   nitro.innerHTML = String(N)
 
    let ph = parseInt(document.getElementById('ph').innerHTML)
    let temp = parseInt(document.getElementById('suhu').innerHTML)
-   //Logic indicator with parameter temp
+   let humidity = parseInt(document.getElementById('humidity').innerHTML)
+
+   //Logic indicator
    indikatorsuhu(temp)
    pHIndicator(ph)
+   humidityIndicator(humidity)
+
+   light_indicator.innerHTML = lightIndicator(amountOfLight)
+   nitroindicator.innerHTML = Nindicator(N)
 
    setTimeout(() => {
       infiniteLoop()
